f5021a0a5d8b60e32f1c42f9fa5067848ab6b421
joseeusebio <joseeusebioeng@gmail.com>
2023-03-08 09:54:54 -0400

    Funcao anonima

---
+++ função/iife.js
@@ -0,0 +1,9 @@
+// IIFE -> Immediately Invoked Function Expression 
+// Expressao de Função Imediantamente Invocada
+
+(function(){
+  console.log('Ser executado na hora')
+  console.log('Foge do escopo mais abrangente!')
+})()
+
+ 
