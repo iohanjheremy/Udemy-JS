86dfc0e290ed379f44283ebfc2836e339c2e70ec
joseeusebio <joseeusebioeng@gmail.com>
2023-03-29 09:01:50 -0400

    atualizando

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+//Código não executável!!
+
+ //Procedural (Função)
+ processamento(v1, v2, v3)
+
+ //Orientação a objeto
+ objeto = {
+    v1,
+    v2,
+    v3,
+    processamento(){
+      //...
+    }
+ }
+ 
+objeto.processamento() //foco passou a ser o objeto
+
+// Principios importantes:
+//1. Abstração
+//2. Encapsulamento
+//3. Herança
+//4. Polimorfismo
